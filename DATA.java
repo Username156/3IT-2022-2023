d851ac5f115ef3b4d30b7cf19c917f8b4c174dd3
luke <lukashelesi56@.com>
2022-09-14 11:16:59 +0200

    Initial commit

---
+++ DATA.java
@@ -0,0 +1,12 @@
+public class DATA {
+    public static public static void main(String[] args) {
+
+        System.out.println("boolean");
+        System.out.println("byte");
+        System.out.println("short");
+        System.out.println("int");
+        System.out.println("long");
+        System.out.println("float");
+        System.out.println("double");
+        System.out.println("char");
+}
---
+++ src/main/java/sk/sosholic/prvypolrok/NewClass.java
@@ -0,0 +1,8 @@
+package sk.sosholic.prvypolrok;
+
+public class NewClass {
+    public static void main(String[] args) {
+            System.out.println("cez leto som bol na prazdninach");
+            System.out.println();
+    }
+}
